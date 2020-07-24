7a6709e308df6cfb88c08159fb182dd1f323c6c6
Paul Breakthrough <paulbreakthru@outlook.com>
2020-07-24 14:45:51 +0100

    Added file content type extensions to index

--- DevProject/ViewModels/IndexViewModel.cs
+++ DevProject/ViewModels/IndexViewModel.cs
@@ -10,7 +10,7 @@ namespace DevProject.ViewModels
 {
     public class IndexViewModel
     {
-        [Requiredo]
+        [Required]
         [Display(Prompt = "Enter Email")]
         public string Email { get; set; }
 
